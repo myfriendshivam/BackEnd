2202ba1194cf05c11d67a9acd8a11ab87ed7f029
myfriendshivam <shivamkant333@gmail.com>
2026-02-26 23:47:55 +0530

    full stack api handling backend

---
+++ full-stack-api/backend/server.js
@@ -0,0 +1,50 @@
+// const express = require('express'); // common JS
+import express from 'express'; // module JS
+
+const app = express();
+
+// app.get('/', (req, res) => {
+//      res.send('Server is ready');
+// });
+
+//get a list of 5 jokes
+
+app.get('/api/jokes', (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: 'A joke',
+            content: 'This is a joke'
+        },
+        {
+            id: 2,
+            title: 'A second joke',
+            content: 'This is a second joke'
+        },
+        {
+            id: 3,
+            title: 'A third joke',
+            content: 'This is a third joke'
+        },
+        {
+            id: 4,
+            title: 'A fourth joke',
+            content: 'This is a fourth joke'
+        },
+        {
+            id: 5,
+            title: 'A fifth joke',
+            content: 'This is a fifth joke'
+        },
+        
+    ];
+    res.send(jokes);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Serve at http://localhost:${port}`);
+});
+
+// Seen the api key with help of JSON formate
---
+++ full-stack-api/frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+// add proxy create react app -> in package.json
+import './App.css'
+
+function App() {
+  const [jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/jokes')
+    .then((response) => {
+      setJokes(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }, [])
+  
+  return (
+    <>
+     <h1>Chai and Full Stack</h1>
+     <p>JOKES: {jokes.length}</p>
+     {
+      jokes.map((joke, index) => (
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <h2>{joke.content}</h2>
+        </div>
+      ))
+     }
+    </>
+  )
+}
+
+export default App
