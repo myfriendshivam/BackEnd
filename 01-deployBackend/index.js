8c90bf2587e2e45045607e0e989bbcef214cc541
myfriendshivam <shivamkant333@gmail.com>
2026-03-03 23:56:03 +0530

    01-deploybackend

---
+++ 01-deployBackend/index.js
@@ -0,0 +1,22 @@
+// console.log("backend");
+
+require('dotenv').config()
+
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>Please login at chai with backend<h1/>')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`);
+})
+
+// dot env package
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Back End with JavaScript
 
-
 <img width="1366" height="850" alt="context api" src="./assets/back-end.webp" />
+
+# Express
+<img width="1366" height="850" alt="context api" src="./assets/express.webp" />
